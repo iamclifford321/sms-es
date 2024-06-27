8729dc5c21756753826db09712cf64667fc445a9
iamclifford321 <iamclifford321@gmail.com>
2024-06-27 16:50:31 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -3,10 +3,10 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function(data){
-    document.getElementById('configuration').value() = JSON.Stringify(data, null, 2)
+    document.getElementById('configuration').value = JSON.Stringify(data, null, 2)
 });
 
 connection.on('clickedNext', function(){
-    var configuration = JSON.parse(document.getElementById('configuration').value());
+    var configuration = JSON.parse(document.getElementById('configuration').value);
     connection.trigger('updateActivity', configuration);
 });
